137320c79cd125598ee1195e64db6ef438bfa0a3
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-21 21:33:48 -0300

    feat: fetch data from api and populate details component

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -7,4 +7,19 @@
   ></dentist-profile-details>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+const route = useRoute();
+const dentistProfileId = computed<string>(() => route.params.id.toString());
+const { data: response, error } = await useFetch<DentistApiReponse, DentistApiError>(DEV_URL.API.concat(dentistProfileId.value));
+
+if (!response.value) {
+  handleDentistNotFoundError(error.value as NonNullable<DentistApiError>);
+}
+
+const { name, picture, role, specialty } = response.value!.dentist as DentistProfile;
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+</style>

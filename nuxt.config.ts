242e10f0fc1e4811c8ede5e3f4e9ca4f0d95c4bb
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:30:42 -0300

    fix: remove nitro cahce control

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,21 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
-    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
-    configPath: "~/tailwind.config.ts",
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
+    configPath: '~/tailwind.config.ts',
     exposeConfig: {
-      level: 2,
+      level: 2
     },
-    viewer: true,
+    viewer: true
   },
   app: {
     head: {
-      title: 'Desafio CATe - João Victor',
-    },
+      title: 'Desafio CATe - João Victor'
+    }
   },
   imports: {
-    dirs: [
-      "enums",
-      "errors",
-      "models",
-      "constants"
-    ]
-  },
-  nitro: {
-    routeRules: {
-      "/images/**": {
-        headers: {
-          'Cache-Control': 'public, max-age=3600'
-        }
-      }
-    }
+    dirs: ['enums', 'errors', 'models', 'constants']
   }
 });

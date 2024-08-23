92f3ce3e78d748d7f804d692e40e2c87cb0680f3
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:31:10 -0300

    feat: add missing tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,27 @@
 import type { Config } from 'tailwindcss';
 
-export default <Partial<Config>> {
-  content: ["./*/**/*.vue"],
+export default <Partial<Config>>{
+  content: ['./*/**/*.vue'],
   theme: {
     extend: {
       colors: {
-        "primary": "#00BBB9",
-        "dark-blue": "#263238",
-        "light-grey": "#ECEFF1"
+        primary: '#00BBB9',
+        'dark-blue': '#263238',
+        'light-grey': '#ECEFF1'
       },
       backgroundImage: {
-        "white-shadow": `
+        'white-shadow': `
           linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, 
             rgba(255, 255, 255, .4) 33%, 
             rgba(255, 255, 255, 0.5) 66%, 
             rgba(255, 255, 255, 1) 100%)
-        ` 
+        `
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif']
       },
       fontSize: {
+        tag: ['12px', '16px'],
         base: ['14px', '24px'],
         md: ['16px', '20px'],
         lg: ['22px', '24px'],
@@ -30,9 +31,9 @@ export default <Partial<Config>> {
         media: '254px',
         mobile: '360px',
         gallery: '778px',
-        text: '980px',        
-        desktop: '1180px',
+        text: '980px',
+        desktop: '1180px'
       }
-    },
-  },
+    }
+  }
 };

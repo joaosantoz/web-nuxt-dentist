bcbf0410730e5a9502a4d250c13990fbbd6f7c09
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:36:49 -0300

    feat: add button load more medias

---
+++ components/gallery/MediaLoad.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-primary w-80 mx-auto group cursor-pointer mt-5 hover:scale-105 transition-all flex items-center justify-center rounded-[4px] h-10 text-white text-center text-md font-semibold">
+    <icons-svg-sync class="group-hover:rotate-180 transition-all" />
+    <p class="text-md font-semibold ml-1">Carregar mais mídias</p>
+  </div>
+</template>

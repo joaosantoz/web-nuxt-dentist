0202f33065dd855c815b25a8b540083be4d550af
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:33:10 -0300

    feat: add new icons

---
+++ components/icons/SvgPlay.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="19"
+    height="24"
+    viewBox="0 0 19 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0 2.50333V21.4967C0 22.945 1.595 23.825 2.82333 23.0367L17.7467 13.54C18.8833 12.825 18.8833 11.175 17.7467 10.4417L2.82333 0.963328C1.595 0.174994 0 1.05499 0 2.50333Z"
+      fill="white" />
+  </svg>
+</template>

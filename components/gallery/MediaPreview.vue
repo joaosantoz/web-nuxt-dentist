5fcf0e4cae1184e412475f29d5348411f6f72301
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:36:11 -0300

    feat: add gallery grid

---
+++ components/gallery/MediaPreview.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex max-w-80 md:max-w-screen-gallery mx-auto flex-wrap items-center justify-start gap-1">
+    <gallery-media-item
+      v-for="media in props.mediaList"
+      :key="media.url"
+      :media-info="media" />
+  </div>
+  <gallery-media-modal />
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ mediaList: MediaInfo[] }>();
+</script>

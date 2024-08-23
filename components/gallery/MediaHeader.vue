94eaf0ce8986c98ac4ada74a730520a03fdf3808
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:35:58 -0300

    feat: add gallery header

---
+++ components/gallery/MediaHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex max-w-60 md:max-w-screen-text md:px-5 md:mx-auto mb-5 items-center sm:max-w-none">
+    <icons-svg-camera class="md:h-9 md:w-9" />
+    <h2 class="text-md font-semibold text-dark-blue ml-2 md:text-lg">Fotos e vídeos de meus pacientes</h2>
+  </div>
+</template>

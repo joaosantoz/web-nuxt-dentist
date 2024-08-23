e305954e0699f974c3466556f80e4bf31e708c4c
João Victor <joaovictor.santos@atlastechnol.com>
2024-08-23 08:36:25 -0300

    feat: add tag selector for gallery

---
+++ components/gallery/MediaSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex items-center w-80 mx-auto justify-between max-w-screen-[300px] my-5">
+    <gallery-media-tag
+      v-for="tag in tagData"
+      :tag-active="checkTagActive(tag.title)"
+      :tag-count="tag.count"
+      :tag-title="tag.title"
+      :key="tag.title"
+      @change-tag="setNewTag" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { activeTag, tagData, setActiveTag } = inject<TagSelectorComposable>(PROVIDER_KEY.TAG_SELECTOR)!;
+
+const emit = defineEmits<{
+  (event: 'filterGallery', tag: TAG_MEDIA): void;
+}>();
+
+const checkTagActive = (tagMedia: TAG_MEDIA): boolean => {
+  return tagMedia === activeTag.value;
+};
+
+const setNewTag = (tagTitle: TAG_MEDIA) => {
+  setActiveTag(tagTitle);
+  emit('filterGallery', tagTitle);
+};
+</script>
---
+++ components/gallery/MediaTag.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative group flex flex-col justify-between items-center w-[104px] h-8">
+    <h1
+      @click="emitEventChangeTag"
+      class="text-tag text-dark-blue text-center cursor-pointer"
+      :class="{ 'font-bold': tagActive }">
+      {{ tagTitle }} {{ lengthText }}
+    </h1>
+    <div
+      class="bg-dark-blue h-1 transition-all duration-500"
+      :class="[tagActive ? 'w-12' : 'w-0']"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  tagActive: boolean;
+  tagCount: number;
+  tagTitle: TAG_MEDIA;
+}>();
+
+const emit = defineEmits<{
+  (event: 'changeTag', tagTitle: TAG_MEDIA): void;
+}>();
+
+const emitEventChangeTag = () => {
+  emit('changeTag', props.tagTitle);
+};
+
+const lengthText = computed<string>(() => `(${props.tagCount})`);
+</script>
